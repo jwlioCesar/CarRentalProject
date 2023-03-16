088030abb6b4be3ff339cda23010aa323fad86e5
Júlio César <juliocfds20@gmail.com>
2023-03-15 23:47:58 -0300

    (no interfaces)

---
+++ src/exceptions/ProblemException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class ProblemException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public ProblemException(String msg) {
+		super(msg);
+	}
+	
+}
